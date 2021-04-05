cce93aa9ce992250bb3969736e203289f97e6c7d
aleucilima <aleuci.lima@gmail.com>
2021-04-05 13:35:29 -0300

    Foodfy update

--- src/scripts.js
+++ src/scripts.js
@@ -4,11 +4,11 @@ const cards = document.querySelectorAll('.card')
 for (let card of cards) {
     card.addEventListener('click', function(){
         const imgId = card.getAttribute('id')
-        
         modalOverlay.classList.add('active')
-        modalOverlay.querySelector('img').src = `/assets/${imgId}`
+        modalOverlay.querySelector('iframe').src = `http://127.0.0.1:5500/assets/${imgId}`
+        console.log(imgId)
     })
-
+    
 }
 
 document.querySelector('.close-modal').addEventListener('click', function(){
