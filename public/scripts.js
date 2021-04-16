c462e4bdac8668a13b608756b5a8d02224e171a8
aleucilima <aleuci.lima@gmail.com>
2021-04-16 14:40:16 -0300

    Update Foodfy

--- public/scripts.js
+++ public/scripts.js
@@ -2,15 +2,9 @@ const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
-    card.addEventListener('click', function(){
-        const imgId = card.getAttribute('id')
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector('iframe').src = `http://127.0.0.1:5500/assets/${imgId}`
-        console.log(imgId)
+    card.addEventListener('click', () => {
+        const recipe = card.getAttribute('id')
+        window.location.href = `https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/${recipe}.png`
     })
     
 }
-
-document.querySelector('.close-modal').addEventListener('click', function(){
-    modalOverlay.classList.remove('active')
-})
--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const app = express()
+const recipe = require('./data')
 
 app.use(express.static('public'))
 
+app.set('view engine', "njk")
+
 nunjucks.configure('views', {
-    express:app
+    express: app,
+    autoescape: false,
+    noCache: true
+})
+
+app.get('/', (request, response) => {
+    return response.render('index', { items: recipe })
 })
 
 app.listen(3333)
