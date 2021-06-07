2d787a2bffc9a29980e842e26b6a43047ed3e03c
aleucilima <aleuci.lima@gmail.com>
2021-06-07 15:04:39 -0300

    Upgrade in the archive scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,15 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener('click', () => {
+        let recipeIndex = card.getAttribute('id')
+        window.location.href = `/recipe/${recipeIndex}`
+
+    })
+}
+
+/*  Function for button "Esconder/Mostrar" */
 const showHides = document.querySelectorAll('.topic')
 
 for(let showHide of showHides) {
