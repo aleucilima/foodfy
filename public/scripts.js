4650743df65732b9e119891127631ccff5ec9a4c
aleucilima <aleuci.lima@gmail.com>
2021-06-07 14:53:02 -0300

    Upgrade in the archives scripts.js and recipe.njk

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,15 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
+const showHides = document.querySelectorAll('.topic')
 
-for (let card of cards) {
-    card.addEventListener('click', () => {
-        let recipeIndex = card.getAttribute('id')
-        window.location.href = `/recipe/${recipeIndex}`
+for(let showHide of showHides) {
+    const btnShowHide = showHide.querySelector('button')
+
+    btnShowHide.addEventListener('click', () => {
+        if(btnShowHide.innerHTML == "ESCONDER") {
+            showHide.querySelector('.topic-content').classList.add('hidden')
+            btnShowHide.innerHTML = "MOSTRAR"
+        } else {
+            showHide.querySelector('.topic-content').classList.remove('hidden')
+            btnShowHide.innerHTML = "ESCONDER"
+        }
     })
-    
 }
