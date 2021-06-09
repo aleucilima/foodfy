8e3e40c5727003506b52655c1cb04e2a41b1f1ac
aleucilima <aleuci.lima@gmail.com>
2021-06-08 21:16:14 -0300

    Upgrade in the scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -10,4 +10,18 @@ for (let card of cards) {
 }
 
 /*  Function for button "Esconder/Mostrar" */
-const showHides = document.querySelectorAll('.topic')
+const showHides = document.querySelectorAll('h4')
+
+for(let i = 0; i < showHides.length; i++) {
+    showHides.item(i).addEventListener('click', () => {
+        let selector = document.querySelectorAll('.topic-content')[i]
+
+        if(showHides.item(i).innerHTML == 'ESCONDER') {
+            showHides.item(i).innerHTML = 'MOSTRAR';
+            selector.style.display = 'none'
+        } else if (showHides.item(i).innerHTML == 'MOSTRAR') {
+            showHides.item(i).innerHTML = 'ESCONDER'
+            selector.style.display = 'block'
+        }
+    })
+}
